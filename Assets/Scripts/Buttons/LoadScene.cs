2503c462a98737b85dd9396c64b9834b68b3804f
pmartinsesa <pmartinsesa@gmail.com>
2024-03-04 15:33:42 -0300

    finish module 13

---
+++ Assets/Scripts/Buttons/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Buttons
+{
+    public class LoadScene : MonoBehaviour
+    {
+        public void Load(string sceneName)
+        {
+            new SceneHandler().LoadSceneByName(sceneName);
+        }
+    }
+}
