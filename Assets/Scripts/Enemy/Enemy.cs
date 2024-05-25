e23b09a76ef36dced9212eb8083ad0fd67eb68cc
pmartinsesa <pmartinsesa@gmail.com>
2024-05-25 16:29:45 -0300

    chore: add death animation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,19 +1,24 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Assets.Scripts.Enemy
 {
     public class Enemy : MonoBehaviour
     {
-        //[Header("Damage settings")]
-       // public UnityEvent enemyLifeBase;
-
+        private int _timeToDeath = 1;
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if(collision.gameObject.tag == "Bullet")
+            if (collision.gameObject.tag == "Bullet")
             {
-                Destroy(gameObject);
+                gameObject.GetComponent<Animator>().SetBool("isDead", true);
+                StartCoroutine(DestroyEnemy());
             }
+        }
+
+        private IEnumerator DestroyEnemy()
+        {
+            yield return new WaitForSeconds(_timeToDeath);
+            Destroy(gameObject);
         }
     }
 }
