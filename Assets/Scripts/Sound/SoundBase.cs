c81c26d616f444978e7ed974393881a1cdbf8a31
pmartinsesa <pmartinsesa@gmail.com>
2024-07-05 12:01:03 -0300

    chore: update sound base

--- Assets/Scripts/Sound/SoundBase.cs
+++ Assets/Scripts/Sound/SoundBase.cs
@@ -7,14 +7,27 @@ namespace Assets.Scripts.Sound
     {
         [Header("Audio Settings")]
         public List<AudioClip> audioClips;
-        public AudioSource audioSource;
+        public AudioSource defaultAudioSource;
 
         public void OnPlay()
         {
-            OnPlayWithAudioSource(audioSource, audioClips[Random.Range(0, audioClips.Count - 1)]);
+            OnPlayMultipleSounds(defaultAudioSource, audioClips[Random.Range(0, audioClips.Count - 1)]);
         }
 
-        public void OnPlayWithAudioSource(AudioSource source, AudioClip clip)
+        public void OnPlayMultipleSounds(AudioSource source, AudioClip clip)
+        {
+            if(!source.isPlaying)
+            {
+                PlaySound(source, clip);
+                return;
+            }
+
+            var newAudioSource = gameObject.AddComponent<AudioSource>();
+            PlaySound(newAudioSource, clip);
+            Destroy(newAudioSource, 1f);
+        }
+
+        private void PlaySound(AudioSource source, AudioClip clip)
         {
             source.clip = clip;
             source.Play();
